67c74e5228a247315e50c35e71ea5824cadfe11c
BillStephens2022 <two4onebill@yahoo.com>
2024-01-29 07:35:14 -0500

    wrote query to get all artists from the DB, successfully tested

--- src/com/billstephens/Main.java
+++ src/com/billstephens/Main.java
@@ -1,6 +1,9 @@
 package com.billstephens;
 
+import com.billstephens.model.Artist;
 import com.billstephens.model.Datasource;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,6 +12,17 @@ public class Main {
             System.out.println("Can't open datasource");
             return;
         }
+
+        List<Artist> artists = datasource.queryArtists();
+        if(artists == null) {
+            System.out.println("No artists found!");
+            return;
+        }
+
+        for(Artist artist : artists) {
+            System.out.println("ID = " + artist.getId() + ", Name = " + artist.getName());
+        }
+
         datasource.close();
     }
 }
--- src/com/billstephens/model/Datasource.java
+++ src/com/billstephens/model/Datasource.java
@@ -1,8 +1,8 @@
 package com.billstephens.model;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Datasource {
 
@@ -50,5 +50,23 @@ public class Datasource {
         }
     }
 
+    public List<Artist> queryArtists() {
+        // try with resources statement will close statement and result set automatically.
+        try(Statement statement = conn.createStatement();
+        ResultSet results = statement.executeQuery("SELECT * FROM " + TABLE_ARTISTS)) {
+            List<Artist> artists = new ArrayList<>();
+            while(results.next()) {
+                Artist artist = new Artist();
+                artist.setId(results.getInt(COLUMN_ARTIST_ID));
+                artist.setName(results.getString(COLUMN_ARTIST_NAME));
+                artists.add(artist);
+            }
 
+            return artists;
+
+        } catch(SQLException e) {
+            System.out.println("Query failed: " + e.getMessage());
+            return null;
+        }
+    }
 }
